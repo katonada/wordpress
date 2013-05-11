b845b0099e41aba4471d164de2c297772b512f3e
katonada <davor.katona@gmail.com>
2013-05-11 08:51:28 +0200

    functions.php

---
+++ functions.php
@@ -0,0 +1,31 @@
+/* Limit Excerpt to 20 - better solution is Advanced Excerpt (http://basvd.com/code/advanced-excerpt/) */
+function custom_excerpt_length( $length ) {
+    return 20;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+
+
+
+/* Add Excerpt to pages */
+function my_add_excerpts_to_pages() {
+     add_post_type_support( 'page', 'excerpt' );
+}
+add_action( 'init', 'my_add_excerpts_to_pages' );
+
+
+
+/* Translate month in archive to croatian */
+function translate_archive_month($list) {
+    $patterns = array(
+        '/January/', '/February/', '/March/', '/April/', '/May/', '/June/',
+        '/July/', '/August/', '/September/', '/October/',  '/November/', '/December/'
+    );
+    $replacements = array(
+        'Siječanj', 'Veljača', 'Ožujak', 'Travanj', 'Svibanj', 'Lipanj', 
+        'Srpanj', 'Kolovoz', 'Rujan', 'Listopad', 'Studeni', 'Prosinac'
+    );    
+    $list = preg_replace($patterns, $replacements, $list);
+    return $list; 
+}
+
+add_filter('get_archives_link', 'translate_archive_month');
